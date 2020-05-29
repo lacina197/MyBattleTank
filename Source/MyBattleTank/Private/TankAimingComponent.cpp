0df4dc6b73a5de7390a1cdd5f893558ab4ae887e
Lacin <lacinacaroglu@gmail.com>
2020-05-29 22:49:12 +0300

    BT p225

--- Source/MyBattleTank/Private/TankAimingComponent.cpp
+++ Source/MyBattleTank/Private/TankAimingComponent.cpp
@@ -8,7 +8,7 @@ UTankAimingComponent::UTankAimingComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = true; // TODO should this really tick?
 
 	// ...
 }
@@ -40,7 +40,15 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 	{
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
 		MoveBarrelTowards(AimDirection);
+		auto Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f: Aim Solution found."), Time);
 	}
+	else
+	{
+		auto Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f: NO Aim Solve found."), Time);
+	}
+	
 	// If no solution found do nothing
 }
 
--- Source/MyBattleTank/Private/TankBarrel.cpp
+++ Source/MyBattleTank/Private/TankBarrel.cpp
@@ -3,10 +3,9 @@
 
 #include "TankBarrel.h"
 
-void UTankBarrel::Elevate(float DegreesPerSecond)
+void UTankBarrel::Elevate(float RelativeSpeed)
 {
     // Move the barrel the right amount this frame
 	// Given a max eleveation speed and the frame rate
-    UE_LOG(LogTemp, Warning, TEXT("degreePerSecond: %f"), DegreesPerSecond);
 }
 
