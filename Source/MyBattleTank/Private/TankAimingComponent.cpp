0064c890deaf88fef280767a98ff0dea3a0f3891
Lacin <lacinacaroglu@gmail.com>
2020-05-31 09:16:43 +0300

    BT p230
    bir Input olarak Fire fonksiyonu eklendi, BP ayarları yapıldı ve şuanda sadece log basıyor.
    Projectile class'ı oluşturuldu. (Actor)

--- Source/MyBattleTank/Private/TankAimingComponent.cpp
+++ Source/MyBattleTank/Private/TankAimingComponent.cpp
@@ -50,8 +50,6 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 	{
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
 		MoveBarrelTowards(AimDirection);
-		auto Time = GetWorld()->GetTimeSeconds();
-		UE_LOG(LogTemp, Warning, TEXT("%f: Aim Solution found."), Time);
 	}
 	else
 	{
